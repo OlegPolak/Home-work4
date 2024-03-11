278fdf4e9eb6b3a51997f879ff1c9fbadc011beb
Oleg Polak <pool42740@gmail.com>
2024-03-11 18:03:48 +0200

    added lesson5

---
+++ js/lesson5.js
@@ -0,0 +1,94 @@
+/**1 */
+
+// const studyInfo = {
+//     name: "name",
+//     specialty: "Cook",
+//     GPA: 10,
+//     missed: 0,
+//     logInfo: function () {
+//         console.group(`${name} info:`);
+//         console.log(`Name is ${this.name}`);
+//         console.log(`Specialty is ${this.specialty}`);
+//         console.log(`GPA is ${this.GPA}`);
+//         console.log(`Missed classes is ${this.missed}`);
+//         console.groupEnd();
+//     },
+// };
+
+// const Vano = {
+//     name: 'Ivan',
+//     specialty: 'Cook',
+//     GPA: 7,
+//     missed: 30,
+// };
+
+// const Olga = {
+//     name: 'Olga',
+//     specialty: 'Stylist',
+//     GPA: 9,
+//     missed: 16,
+// };
+
+// const Kate = {
+//     name: 'Kate',
+//     specialty: 'Veterinarian',
+//     GPA: 10,
+//     missed: 3,
+// };
+
+// const Mary = {
+//     name: 'Mary',
+//     specialty: 'Teacher',
+//     GPA: 11,
+//     missed: 5,
+// };
+
+// studyInfo.logInfo.call(Vano);
+// studyInfo.logInfo.call(Olga);
+// studyInfo.logInfo.call(Kate);
+// studyInfo.logInfo.call(Mary);
+
+// studyInfo.logInfo.apply(Vano);
+// studyInfo.logInfo.apply(Olga);
+// studyInfo.logInfo.apply(Kate);
+// studyInfo.logInfo.apply(Mary);
+
+// studyInfo.logInfo.bind(Vano)();
+// studyInfo.logInfo.bind(Olga)();
+// studyInfo.logInfo.bind(Kate)();
+// studyInfo.logInfo.bind(Mary)();
+
+
+/**2 */
+
+// const definition = {
+//     showItem() {
+// console.log('Визначення', this.item);
+//     }
+// };
+
+// const htmlInfo = {
+//     item: ["HTML-стандартизована мова розмітки документів для перегляду вебсторінок у браузері."],
+// };
+// const cssInfo = {
+//     item: ["CSS-це спеціальна мова стилю сторінок, що використовується для опису їхнього зовнішнього вигляду."],
+// };
+
+// document.querySelector('#html').addEventListener('click', definition.showItem.bind(htmlInfo));
+// document.querySelector('#css').addEventListener('click', definition.showItem.bind(cssInfo));
+
+/**3 */
+
+// banana 30, 4,5/ 135
+// 2) cherry 58, 1,3/75.4
+// 3) jrange 89. 3,4/302.6
+
+// price
+// weight
+// cost
+
+// const goods= {
+//     showItem() {
+//         console.log(`Товар ${name} Вартість ${cost}`);
+//     }
+// };
