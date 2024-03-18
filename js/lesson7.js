3b5fe20dcb735329508e586f2f4b17f77bd6f994
Oleg Polak <pool42740@gmail.com>
2024-03-18 15:16:20 +0200

    new lesson7

--- js/lesson7.js
+++ js/lesson7.js
@@ -1,21 +1,21 @@
-function Calculator() {
+// function Calculator() {
 
-    this.read = function () {
-        this.x = +prompt('Напишіть перше будь яке число');
-        this.y = +prompt('Напишіть друге будь яке число');
-    }
+//     this.read = function () {
+//         this.x = +prompt('Напишіть перше будь яке число');
+//         this.y = +prompt('Напишіть друге будь яке число');
+//     }
     
-    this.sum = function () {
-        return this.x + this.y;
-    }
+//     this.sum = function () {
+//         return this.x + this.y;
+//     }
 
-    this.mul = function () {
-        return this.x * this.y;
-    }
-}
+//     this.mul = function () {
+//         return this.x * this.y;
+//     }
+// }
 
-let calculator = new Calculator();
-calculator.read();
+// let calculator = new Calculator();
+// calculator.read();
 
-alert('Сума двох чисел = ' + calculator.sum());
-alert('Добуток двох чисел = ' + calculator.mul());
+// alert('Сума двох чисел = ' + calculator.sum());
+// alert('Добуток двох чисел = ' + calculator.mul());
